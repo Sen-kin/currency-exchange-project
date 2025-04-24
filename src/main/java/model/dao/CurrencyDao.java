bcf2262ee2096c2df8a651b259036939e5ecfaef
Arseniy <arseniyg2806@gmail.com>
2025-04-24 16:35:56 +0300

    Replace @SneakyThrows with try/catch and add validation to builder method

---
+++ src/main/java/model/dao/CurrencyDao.java
@@ -0,0 +1,54 @@
+package model.dao;
+
+import model.DataBaseIsNotAvailibleExeption;
+import model.entity.CurrencyEntity;
+import lombok.SneakyThrows;
+import util.ConnectionManager;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class CurrencyDao implements CurrencyCRUD<String, CurrencyEntity>{
+
+    private static final String FIND_BY_CODE = "SELECT * FROM Currencies WHERE Code=?";
+    private static final CurrencyDao INSTANCE = new CurrencyDao();
+
+    private CurrencyDao(){}
+
+
+    @Override
+    public Optional<CurrencyEntity> findByCode(String code) throws DataBaseIsNotAvailibleExeption {
+        try(var connection = ConnectionManager.get();
+        var statement = connection.prepareStatement(FIND_BY_CODE)){
+
+            statement.setString(1, code);
+
+            var result = statement.executeQuery();
+
+            return Optional.ofNullable(builder(result));
+        } catch (SQLException e) {
+            throw new DataBaseIsNotAvailibleExeption(e);
+        }
+
+    }
+
+
+    @SneakyThrows
+    private CurrencyEntity builder(ResultSet resultSet){
+
+        if (resultSet.getString("Code") == null) return null;
+
+        return new CurrencyEntity(
+                    resultSet.getLong("ID"),
+                    resultSet.getString("Code"),
+                    resultSet.getString("FullName"),
+                    resultSet.getString("Sign")
+    );
+
+    }
+
+    public static CurrencyDao getInstance() {
+        return INSTANCE;
+    }
+}
