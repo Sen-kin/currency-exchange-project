a6903e6d4592c568bb89d51b3b001af00ec6f7b1
Arseniy <arseniyg2806@gmail.com>
2025-04-25 00:17:52 +0300

    fix typo

--- src/main/java/model/dao/CurrencyCRUD.java
+++ src/main/java/model/dao/CurrencyCRUD.java
@@ -1,11 +1,11 @@
 package model.dao;
 
-import model.DataBaseIsNotAvailibleExeption;
+import model.DataBaseIsNotAvalibleExeption;
 import model.InvalidCodeExeption;
 
 import java.util.Optional;
 
 public interface CurrencyCRUD<K, T> {
 
-    Optional<T> findByCode(K code) throws InvalidCodeExeption, DataBaseIsNotAvailibleExeption;
+    Optional<T> findByCode(K code) throws InvalidCodeExeption, DataBaseIsNotAvalibleExeption;
 }
