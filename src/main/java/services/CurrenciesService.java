c94bcd435856b80b5ea2146a38a0aa27ddd9d9eb
Arseniy <arseniyg2806@gmail.com>
2025-04-24 16:33:14 +0300

    Create method save to create new Currency in Data base. Also add exeptions to methods' signatures.

---
+++ src/main/java/services/CurrenciesService.java
@@ -0,0 +1,65 @@
+package services;
+
+import model.CurrencyAlreadyExistExeption;
+import model.DataBaseIsNotAvailibleExeption;
+import model.dto.CurrencyDto;
+import model.dao.CurrenciesDao;
+import model.entity.CurrencyEntity;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class CurrenciesService {
+
+    private static final CurrenciesService INSTANCE = new CurrenciesService();
+
+    private final CurrenciesDao currenciesDao = CurrenciesDao.getInstance();
+
+    private CurrenciesService(){}
+
+    public List<CurrencyDto> findAll() throws ServiceExeption{
+        try {
+            return currenciesDao
+                    .findAll()
+                    .stream()
+                    .map(dao -> new CurrencyDto(
+                                    dao.getId(),
+                                    dao.getCode(),
+                                    dao.getFullName(),
+                                    dao.getSign()
+                            )
+                    )
+                    .collect(Collectors.toList());
+        } catch (DataBaseIsNotAvailibleExeption e) {
+            throw new ServiceExeption(e);
+        }
+    }
+
+    public CurrencyDto createCurrency(CurrencyDto currencyDto) throws DataBaseIsNotAvailibleExeption, CurrencyAlreadyExistExeption {
+
+        Optional<CurrencyEntity> createdEntity = currenciesDao.save(
+                new CurrencyEntity(currencyDto.id(),
+                        currencyDto.code(),
+                        currencyDto.name(),
+                        currencyDto.sign()
+                ));
+
+        if (createdEntity.isPresent()) {
+
+            CurrencyEntity newCurrency = createdEntity.get();
+            return new CurrencyDto(
+                    newCurrency.getId(),
+                    newCurrency.getCode(),
+                    newCurrency.getFullName(),
+                    newCurrency.getSign()
+            );
+        }
+        return new CurrencyDto(null, "", "", "");
+    }
+
+    public static CurrenciesService getInstance() {
+        return INSTANCE;
+    }
+
+}
