4544d4db6d22f0dc91468218dcc2afd5f556ebfc
Arseniy <arseniyg2806@gmail.com>
2025-04-25 23:11:49 +0300

    Add exeptions to methods' signatures

--- src/main/java/model/dao/ExchangeRatesCRUD.java
+++ src/main/java/model/dao/ExchangeRatesCRUD.java
@@ -2,6 +2,9 @@ package model.dao;
 
 
 import model.DataBaseIsNotAvalibleExeption;
+import model.ExchangeRateAlreadyExistExeption;
+import model.ExchangeRateCodeDoesNotExistExeption;
+import model.dto.ExchangeRatesDto;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,7 +15,7 @@ public interface ExchangeRatesCRUD<K, T> {
 
      Optional<T> findExchangeRate(K firstID, K secondID) throws DataBaseIsNotAvalibleExeption;
 
-     void createExchangeRate(T entity);
+     Optional<ExchangeRatesDto> createExchangeRate(T entity) throws DataBaseIsNotAvalibleExeption, ExchangeRateAlreadyExistExeption, ExchangeRateCodeDoesNotExistExeption;
 
 
 }
