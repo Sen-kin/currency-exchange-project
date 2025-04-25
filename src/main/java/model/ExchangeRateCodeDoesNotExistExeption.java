6f0f0929f633b57919aac11b0628489756ddb13e
Arseniy <arseniyg2806@gmail.com>
2025-04-25 20:24:32 +0300

    add exeption to check if codes of Currencies exist in DB

---
+++ src/main/java/model/ExchangeRateCodeDoesNotExistExeption.java
@@ -0,0 +1,8 @@
+package model;
+
+import java.sql.SQLException;
+
+public class ExchangeRateCodeDoesNotExistExeption extends Throwable {
+    public ExchangeRateCodeDoesNotExistExeption(sqlexception e) {
+    }
+}
