70d1d53a52afc1756b978861b7c1eb03f4d08ae3
Arseniy <arseniyg2806@gmail.com>
2025-04-25 23:14:17 +0300

    make ExchangeRatesDto record instead of using Lombok annotations

--- src/main/java/model/dto/ExchangeRatesDto.java
+++ src/main/java/model/dto/ExchangeRatesDto.java
@@ -5,13 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@AllArgsConstructor
-@Setter
-@Getter
-public class ExchangeRatesDto {
 
-    private Long id;
-    private CurrencyDto baseCurrency;
-    private CurrencyDto targetCurrency;
-    private Double rate;
-}
+public record ExchangeRatesDto(
+        Long id,
+        CurrencyDto baseCurrency,
+        CurrencyDto targetCurrency,
+        Double rate) { }
