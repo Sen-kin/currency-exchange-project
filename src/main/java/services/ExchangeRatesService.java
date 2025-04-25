21008f41e74a14f0aa2b4ef65ad7f93733e64d17
Arseniy <arseniyg2806@gmail.com>
2025-04-25 23:14:46 +0300

    add createExchangeRate method

--- src/main/java/services/ExchangeRatesService.java
+++ src/main/java/services/ExchangeRatesService.java
@@ -1,8 +1,11 @@
 package services;
 
 import model.DataBaseIsNotAvalibleExeption;
+import model.ExchangeRateAlreadyExistExeption;
+import model.ExchangeRateCodeDoesNotExistExeption;
 import model.ExchangeRateIsNotExistExeption;
 import model.dao.ExchangeRatesDao;
+import model.dto.CurrencyDto;
 import model.dto.ExchangeRatesDto;
 
 import java.util.List;
@@ -20,10 +23,17 @@ public class ExchangeRatesService {
         return exchangeRatesDao.findAllExchangeRates();
     }
 
-    public ExchangeRatesDto findExchangeRateByCodes(String baseCode, String targetCode)
+    public ExchangeRatesDto findExchangeRate(String baseCode, String targetCode)
             throws DataBaseIsNotAvalibleExeption, ExchangeRateIsNotExistExeption {
 
         return exchangeRatesDao.findExchangeRate(baseCode, targetCode).orElseThrow(ExchangeRateIsNotExistExeption::new);
+    }
+
+    public ExchangeRatesDto createExchangeRate(String baseCurrencyCode, String targetCurrencyCode, Double rate) throws DataBaseIsNotAvalibleExeption, ExchangeRateAlreadyExistExeption, ExchangeRateCodeDoesNotExistExeption {
+        return exchangeRatesDao.createExchangeRate(new ExchangeRatesDto(null,
+                new CurrencyDto(null, baseCurrencyCode, null, null),
+                new CurrencyDto(null, targetCurrencyCode, null, null),
+                rate)).orElseThrow(ExchangeRateAlreadyExistExeption::new);
     }
 
 
