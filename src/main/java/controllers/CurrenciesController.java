c876ca49a0ffa6d913f5f831750ed30ad7c773b6
Arseniy <arseniyg2806@gmail.com>
2025-04-26 18:03:11 +0300

    Remake exception handling

--- src/main/java/controllers/CurrenciesController.java
+++ src/main/java/controllers/CurrenciesController.java
@@ -2,8 +2,9 @@ package controllers;
 
 import java.io.*;
 
-import model.CurrencyAlreadyExistExeption;
-import model.DataBaseIsNotAvalibleExeption;
+import model.exceptions.CurrencyAlreadyExistsException;
+import model.exceptions.CurrencyCreationException;
+import model.exceptions.DataBaseIsNotAvalibleException;
 import model.dto.CurrencyDto;
 import model.dto.ErrorDto;
 import services.CurrenciesService;
@@ -11,7 +12,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-import services.ServiceExeption;
 
 @WebServlet(value = "/currencies")
 public class CurrenciesController extends HttpServlet {
@@ -21,10 +21,14 @@ public class CurrenciesController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("application/json");
+
         try {
-            resp.setContentType("application/json");
+
             mapper.writeValue(resp.getWriter(), currenciesService.findAll());
-        } catch (ServiceExeption e) {
+
+        } catch (DataBaseIsNotAvalibleException e) {
             resp.setStatus(500);
             mapper.writeValue(resp.getWriter(), new ErrorDto("Ошибка доступа к базе данных"));
         }
@@ -35,26 +39,31 @@ public class CurrenciesController extends HttpServlet {
 
         resp.setContentType("application/json");
 
-            String name = req.getParameter("name");
-            String code = req.getParameter("code").toUpperCase();
-            String sign = req.getParameter("sign");
+        String name = req.getParameter("name");
+        String code = req.getParameter("code");
+        String sign = req.getParameter("sign");
 
-            if (code.isEmpty() || name.isEmpty() || sign.isEmpty()){
-                mapper.writeValue(resp.getWriter(), new ErrorDto("Отсутствует нужное поле формы"));
-            }
+        if (code == null || name == null || sign == null || code.isEmpty() || name.isEmpty() || sign.isEmpty())
+        {
+            mapper.writeValue(resp.getWriter(), new ErrorDto("Отсутствует нужное поле формы"));
+        }
 
-            try {
-               CurrencyDto newCurrency = currenciesService.createCurrency(new CurrencyDto(null, code, name, sign));
-                mapper.writeValue(resp.getWriter(), newCurrency);
-            } catch (CurrencyAlreadyExistExeption e){
-                resp.setStatus(409);
-                mapper.writeValue(resp.getWriter(), new ErrorDto("Валюта с таким кодом уже существует"));
-            } catch (DataBaseIsNotAvalibleExeption e) {
-                resp.setStatus(500);
-                mapper.writeValue(resp.getWriter(), new ErrorDto("Ошибка доступа к базе данных"));
+        try {
 
-            }
+            CurrencyDto newCurrency = currenciesService.createCurrency(new CurrencyDto(null, code, name, sign));
 
+            mapper.writeValue(resp.getWriter(), newCurrency);
+
+        } catch (CurrencyCreationException e) {
+            resp.setStatus(400);
+            mapper.writeValue(resp.getWriter(), new ErrorDto("Отсутствует нужное поле формы"));
+        } catch (CurrencyAlreadyExistsException e){
+            resp.setStatus(409);
+            mapper.writeValue(resp.getWriter(), new ErrorDto("Валюта с таким кодом уже существует"));
+        } catch (DataBaseIsNotAvalibleException e) {
+            resp.setStatus(500);
+            mapper.writeValue(resp.getWriter(), new ErrorDto("Ошибка доступа к базе данных"));
+        }
     }
  }
 
