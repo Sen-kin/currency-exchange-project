e0599e8efa9bc6fbe5120d1c162a8996e4014800
Arseniy <arseniyg2806@gmail.com>
2025-04-27 16:05:54 +0300

    create method exchange

--- src/main/java/services/ExchangeRatesService.java
+++ src/main/java/services/ExchangeRatesService.java
@@ -2,6 +2,7 @@ package services;
 
 import model.dao.ExchangeRateDao;
 import model.dto.CurrencyDto;
+import model.dto.ExchangeDto;
 import model.dto.ExchangeRateDto;
 import model.exceptions.*;
 
@@ -15,19 +16,8 @@ public class ExchangeRatesService {
 
     private ExchangeRatesService() {}
 
-
-    public List<ExchangeRateDto> findAllExchangeRates() throws DataBaseIsNotAvalibleException {
-        return exchangeRatesDao.findAll();
-    }
-
-    public ExchangeRateDto findExchangeRate(String baseCode, String targetCode)
-            throws DataBaseIsNotAvalibleException, ExchangeRateDoesNotExistException {
-
-        return exchangeRatesDao.findByCodes(baseCode, targetCode);
-    }
-
     public ExchangeRateDto createExchangeRate(String baseCurrencyCode, String targetCurrencyCode, Double rate)
-            throws DataBaseIsNotAvalibleException, ExchangeRateAlreadyExistsException, ExchangeRateCodeDoesNotExistException, ExchangeRateCreationException {
+            throws DataBaseIsNotAvailableException, ExchangeRateAlreadyExistsException, ExchangeRateCodeDoesNotExistException, ExchangeRateCreationException {
 
         return exchangeRatesDao.create(
                 new ExchangeRateDto(
@@ -39,8 +29,18 @@ public class ExchangeRatesService {
         );
     }
 
-    public ExchangeRateDto updateExchangeRate(Double rate, String baseCurrencyCode, String targetCurrencyCode)
-            throws DataBaseIsNotAvalibleException, ExchangeRateDoesNotExistException {
+    public List<ExchangeRateDto> findAllExchangeRates() throws DataBaseIsNotAvailableException {
+        return exchangeRatesDao.findAll();
+    }
+
+    public ExchangeRateDto findExchangeRate(String baseCode, String targetCode)
+            throws DataBaseIsNotAvailableException, ExchangeRateDoesNotExistException {
+
+        return exchangeRatesDao.findByCodes(baseCode, targetCode);
+    }
+
+    public ExchangeRateDto updateExchangeRate(String baseCurrencyCode, String targetCurrencyCode, Double rate)
+            throws DataBaseIsNotAvailableException, ExchangeRateDoesNotExistException {
         return exchangeRatesDao.update(new ExchangeRateDto(
                 null,
                 new CurrencyDto(null, baseCurrencyCode, null, null),
@@ -50,6 +50,74 @@ public class ExchangeRatesService {
         );
     }
 
+    public ExchangeDto exchange(String from, String to, Double amount) throws DataBaseIsNotAvailableException, ExchangeRateDoesNotExistException{
+
+       try {
+
+           ExchangeRateDto AB = INSTANCE.findExchangeRate(from, to);
+           return new ExchangeDto(
+                   new CurrencyDto(
+                           AB.baseCurrency().id(),
+                           AB.baseCurrency().name(),
+                           AB.baseCurrency().code(),
+                           AB.baseCurrency().sign()
+                           ),
+                   new CurrencyDto(
+                           AB.targetCurrency().id(),
+                           AB.targetCurrency().name(),
+                           AB.targetCurrency().code(),
+                           AB.targetCurrency().sign()
+                   ),
+                   AB.rate(), amount, AB.rate() * amount
+           );
+
+       } catch (ExchangeRateDoesNotExistException e) {
+           try {
+
+               ExchangeRateDto BA = INSTANCE.findExchangeRate(to, from);
+
+               return new ExchangeDto(
+                       new CurrencyDto(
+                               BA.baseCurrency().id(),
+                               BA.baseCurrency().name(),
+                               BA.baseCurrency().code(),
+                               BA.baseCurrency().sign()
+                       ),
+                       new CurrencyDto(
+                               BA.targetCurrency().id(),
+                               BA.targetCurrency().name(),
+                               BA.targetCurrency().code(),
+                               BA.targetCurrency().sign()
+                       ),
+                       BA.rate(), amount, BA.rate() * amount
+               );
+
+           } catch (ExchangeRateDoesNotExistException e2){
+
+               ExchangeRateDto A_USD = INSTANCE.findExchangeRate(from, "USD");
+                   ExchangeRateDto USD_B = INSTANCE.findExchangeRate("USD", to);
+
+                   return new ExchangeDto(
+                           new CurrencyDto(
+                                   A_USD.baseCurrency().id(),
+                                   A_USD.baseCurrency().name(),
+                                   A_USD.baseCurrency().code(),
+                                   A_USD.baseCurrency().sign()
+                           ),
+                           new CurrencyDto(
+                                   USD_B.targetCurrency().id(),
+                                   USD_B.targetCurrency().name(),
+                                   USD_B.targetCurrency().code(),
+                                   USD_B.targetCurrency().sign()
+                           ),
+                           USD_B.rate() / A_USD.rate(), amount,(USD_B.rate() / A_USD.rate()) * amount
+
+
+                   );
+
+           }
+       }
+    }
 
     public static ExchangeRatesService getInstance() {
         return INSTANCE;
