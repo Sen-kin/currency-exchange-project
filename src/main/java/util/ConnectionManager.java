13dfe5919f962c59580a8247953d481e340a852e
Arseniy <arseniyg2806@gmail.com>
2025-04-28 00:24:07 +0300

    put db in resource root and add application.properties file to easily configure it

--- src/main/java/util/ConnectionManager.java
+++ src/main/java/util/ConnectionManager.java
@@ -6,7 +6,6 @@ import com.zaxxer.hikari.HikariDataSource;
 import lombok.experimental.UtilityClass;
 
 
-import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -15,11 +14,12 @@ public class ConnectionManager {
 
         private static final HikariConfig config = new HikariConfig();
         private static final HikariDataSource dataSource;
+        private static final String URL = "db.url";
 
         static {
-            config.setJdbcUrl("jdbc:sqlite:C:\\Users\\User\\IdeaProjects\\currency-exchange\\identifier.sqlite");
+            config.setJdbcUrl(PropertiesUtil.get(URL));
             config.setDriverClassName("org.sqlite.JDBC");
-            config.setMaximumPoolSize(10);
+            config.setMaximumPoolSize(5);
             config.setConnectionTimeout(5000);
             config.setIdleTimeout(60000);
             config.setMaxLifetime(1800000);
