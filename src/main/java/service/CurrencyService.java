31daf409505c8987f1bda9e765bcd06b3ac46958
Arseniy <arseniyg2806@gmail.com>
2025-04-30 19:30:05 +0300

    finish all methods (add some exception handling related with DB) and add mappers to map objects more comfortable and smaller

---
+++ src/main/java/service/CurrencyService.java
@@ -0,0 +1,43 @@
+package service;
+
+import exception.CurrencyDoesNotExistException;
+import model.dto.CurrencyDTO;
+import repository.CurrencyRepository;
+import model.entity.Currency;
+import util.CurrencyMapper;
+
+import java.util.List;
+
+public class CurrencyService {
+
+    private final CurrencyRepository currencyRepository;
+    private final CurrencyMapper currencyMapper;
+
+    public CurrencyService(CurrencyRepository currenciesDao) {
+        this.currencyRepository = currenciesDao;
+        this.currencyMapper = CurrencyMapper.INSTANCE;
+    }
+
+    public List<CurrencyDTO> findAll() {
+        List<Currency> currencies = currencyRepository.findAll();
+
+        return currencyMapper.toCurrencyDTOList(currencies);
+    }
+
+    public CurrencyDTO find(String code) {
+        Currency currency = currencyRepository.find(code);
+
+        if (currency == null) {
+            throw new CurrencyDoesNotExistException("Currency not found");
+        }
+
+        return currencyMapper.toCurrencyDTO(currency);
+    }
+
+    public CurrencyDTO create(CurrencyDTO currencyDTO) {
+        Currency currency = currencyMapper.toCurrency(currencyDTO);
+        Currency savedCurrency = currencyRepository.create(currency);
+
+        return currencyMapper.toCurrencyDTO(savedCurrency);
+    }
+}
---
+++ src/main/java/service/ExchangeRateService.java
@@ -0,0 +1,95 @@
+package service;
+
+import model.dto.CurrencyDTO;
+import model.entity.Currency;
+import model.entity.ExchangeRate;
+import repository.CurrencyRepository;
+import repository.ExchangeRateRepository;
+import model.dto.ExchangeDTO;
+import model.dto.ExchangeRateDTO;
+import exception.*;
+import util.CurrencyMapper;
+import util.ExchangeRateMapper;
+
+import java.util.List;
+
+public class ExchangeRateService {
+
+    private final CurrencyRepository currencyRepository;
+    private final ExchangeRateRepository exchangeRateRepository;
+    private final CurrencyMapper currencyMapper;
+    private final ExchangeRateMapper exchangeRateMapper;
+    private final String crossRateCurrency;
+
+    public ExchangeRateService(ExchangeRateRepository exchangeRateRepository, CurrencyRepository currencyRepository, String crossRateCurrency) {
+        this.currencyRepository = currencyRepository;
+        this.exchangeRateRepository = exchangeRateRepository;
+        this.currencyMapper = CurrencyMapper.INSTANCE;
+        this.exchangeRateMapper = ExchangeRateMapper.INSTANCE;
+        this.crossRateCurrency = crossRateCurrency;
+    }
+
+    public List<ExchangeRateDTO> findAll() {
+        List<ExchangeRate> exchangeRates = exchangeRateRepository.findAll();
+
+        return exchangeRateMapper.toExchangeRateDTOList(exchangeRates);
+    }
+
+    public ExchangeRateDTO find(String baseCurrencyCode, String targetCurrencyCode) {
+        ExchangeRate foundedExchangeRate = exchangeRateRepository.find(baseCurrencyCode, targetCurrencyCode);
+
+        if (foundedExchangeRate == null) throw new ExchangeRateDoesNotExistException("Exchange rate not found");
+
+        return exchangeRateMapper.toExchangeRateDTO(foundedExchangeRate);
+    }
+
+    public ExchangeRateDTO create(String baseCurrencyCode, String targetCurrencyCode, Double rate) {
+        Currency baseCurrency = currencyRepository.find(baseCurrencyCode);
+        Currency targetCurrency = currencyRepository.find(targetCurrencyCode);
+
+        if (baseCurrency == null || targetCurrency == null)
+            throw new CurrencyDoesNotExistException("Currency not found");
+
+        ExchangeRate createdExchangeRate = exchangeRateRepository.create(new ExchangeRate(null, baseCurrency, targetCurrency, rate));
+
+        return exchangeRateMapper.toExchangeRateDTO(createdExchangeRate);
+    }
+
+    public ExchangeRateDTO update(String baseCurrencyCode, String targetCurrencyCode, Double rate) {
+        ExchangeRate updatedExchangeRate = exchangeRateRepository.update(baseCurrencyCode, targetCurrencyCode, rate);
+
+        return exchangeRateMapper.toExchangeRateDTO(updatedExchangeRate);
+    }
+
+    public ExchangeDTO exchange(String baseCurrencyCode, String targetCurrencyCode, Double amount) {
+        Double rate;
+        ExchangeRate exchangeRate = exchangeRateRepository.find(baseCurrencyCode, targetCurrencyCode);
+
+        if (exchangeRate == null) {
+            ExchangeRate reverseRate = exchangeRateRepository.find(targetCurrencyCode, baseCurrencyCode);
+            if (reverseRate == null) {
+                ExchangeRate baseToIntermediate = exchangeRateRepository.find(baseCurrencyCode, crossRateCurrency);
+                ExchangeRate intermediateToTarget = exchangeRateRepository.find(crossRateCurrency, targetCurrencyCode);
+                if (baseToIntermediate == null || intermediateToTarget == null) {
+                    throw new ExchangeRateDoesNotExistException("Failed to exchange");
+                }
+                rate = intermediateToTarget.getRate() * baseToIntermediate.getRate();
+            } else rate = 1.0 / reverseRate.getRate();
+        } else rate = exchangeRate.getRate();
+
+        Double convertedAmount = rate * amount;
+
+        Currency baseCurrency = currencyRepository.find(baseCurrencyCode);
+        Currency targetCurrency = currencyRepository.find(targetCurrencyCode);
+
+        if (baseCurrency == null || targetCurrency == null) {
+            throw new CurrencyDoesNotExistException("Currency not found");
+        }
+
+        CurrencyDTO baseCurrencyDTO = currencyMapper.toCurrencyDTO(baseCurrency);
+        CurrencyDTO targetCurrencyDTO = currencyMapper.toCurrencyDTO(targetCurrency);
+
+        return new ExchangeDTO(baseCurrencyDTO, targetCurrencyDTO, rate, amount, convertedAmount);
+    }
+}
+
